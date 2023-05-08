b461937fc754c6f308593253efd9f2f6c96d07f9
0xTraderTrou <khangtran.btc@gmail.com>
2023-05-08 21:42:10 +0700

    feat: enabled evm environment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'create-empty-pool.dto.ts',
+    project: 'tsconfig.json',
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
