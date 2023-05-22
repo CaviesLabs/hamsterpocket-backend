e11e91ad37c362ecbf09e2f615d80af42e7ffcd6
0xTraderTrou <khangtran.btc@gmail.com>
2023-05-22 17:27:14 +0700

    fix: fixed required fields for solana

--- src/providers/solana-pocket-program/pocket.oc-dto.ts
+++ src/providers/solana-pocket-program/pocket.oc-dto.ts
@@ -191,5 +191,6 @@ export function convertToPoolEntity(
     // Below fields will be updated once the events data is computed
     currentSpentBaseToken: 0,
     currentReceivedTargetToken: 0,
+    ammRouterAddress: '',
   } as Partial<PoolEntity>);
 }
